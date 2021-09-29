da14526ace4cbcde9e50a342af6f2ac8db7d0738
Alexey Khit <alexey.khit@gmail.com>
2021-09-29 17:04:03 +0300

    Adds raise error function (for tests)

--- device_gateway.go
+++ device_gateway.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"errors"
 	"github.com/AlexxIT/gw3/dict"
+	"github.com/rs/zerolog/log"
 )
 
 type GatewayDevice struct {
@@ -55,4 +57,29 @@ func (d *GatewayDevice) updateBT(fw string, addr uint16, ivi uint32) {
 	d.BT.Addr = addr
 	d.BT.IVIndex = ivi
 	mqttPublish("gw3/"+d.WiFi.MAC+"/info", d, true)
+}
+
+func (d *GatewayDevice) getState() {
+	// BLE device can't get state
+}
+
+func (d *GatewayDevice) setState(p []byte) {
+	payload, err := dict.Unmarshal(p)
+	if err != nil {
+		log.Warn().Err(err).Send()
+		return
+	}
+	if value, ok := payload.TryGetString("test"); ok {
+		switch value {
+		case "error":
+			// raise unhandled error
+			devices["test"].(DeviceGetSet).getState()
+		case "fatal":
+			err = errors.New("test")
+			log.Fatal().Caller().Err(err).Send()
+		case "panic":
+			err = errors.New("test error")
+			log.Panic().Err(err).Send()
+		}
+	}
 }
